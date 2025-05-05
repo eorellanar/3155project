61e236b956e21eec9446013b89b30e6382bb64fd
eorellanar <edgar_geo04@icloud.com>
2025-05-05 13:19:20 -0400

    removed outside api folder

--- api/models/ratings_reviews.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from ..dependecies.database import Base
-
-class RatingsReview(Base):
-    __tablename__ = 'ratings_reviews'
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    review_text = Column(String(500))
-    rating = Column(Integer)
