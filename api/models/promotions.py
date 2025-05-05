ddd5278972b8cd1fff60df2484567e9d8e7803e8
eorellanar <edgar_geo04@icloud.com>
2025-05-05 13:19:20 -0400

    added routers in routers folder

--- api/models/promotions.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, DateTime
-from sqlalchemy.orm import relationship
-from ..dependecies.database import Base
-
-class Promotion(Base):
-    __tablename__ = 'promotions'
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    promotion_code = Column(Integer)
-    expiration_date = Column(DateTime)
--- api/routers/indexRoute.py
+++
@@ -1,6 +0,0 @@
-def load_routes(app):
-    @app.get("/")
-    def read_root():
-        return {"message": "Welcome to the API"}
-
-# Basic API
